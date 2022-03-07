a2fe076879168a088d19a549cef8c7ddd7feccc2
Lasserini <lasserini@gmail.com>
2022-03-07 20:33:52 +0000

    Add authentication and related sites styling

--- kidtstravel/urls.py
+++ kidtstravel/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('activities.urls'), name='activity.urls')
+    path('', include('activities.urls'), name='activity.urls'),
+    path('accounts/', include('allauth.urls')),
 ]
