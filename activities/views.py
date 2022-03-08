26ba09bc1f8443f111cdcf9ffc0b3e93198ffc39
Lasserini <lasserini@gmail.com>
2022-03-08 06:45:30 +0000

    add reviews form and review views

--- activities/views.py
+++ activities/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Experience, Review
 from .filters import ExperienceFilter
+from .forms import ReviewForm
 
 
 # The Index Search page view
@@ -32,6 +33,32 @@ class ExperienceDetail(View):
             "experience_detail.html",
             {
                 "experience": experience,
-                "reviews": reviews
-            }
+                "reviews": reviews,
+                "review_form": ReviewForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Experience.objects.all()
+        experience = get_object_or_404(queryset, slug=slug)
+        reviews = experience.reviews.all().order_by('-created_on')
+
+        review_form = ReviewForm(data=request.POST)
+
+        if review_form.is_valid():
+            review_form.instance.username = request.user.username
+            review = review_form.save(commit=False)
+            review.experience = experience
+            review.save()
+        else:
+            review_form = ReviewForm()
+
+        return render(
+            request,
+            "experience_detail.html",
+            {
+                "experience": experience,
+                "reviews": reviews,
+                "review_form": ReviewForm()
+            },
         )
