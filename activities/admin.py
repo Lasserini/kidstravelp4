36c5723fc7774eba1702d5731f9182aa8902c607
Lasserini <lasserini@gmail.com>
2022-04-13 20:40:02 +0000

    Add admin panel functionality

--- activities/admin.py
+++ activities/admin.py
@@ -2,16 +2,24 @@ from django.contrib import admin
 from .models import Experience, Review
 from django_summernote.admin import SummernoteModelAdmin
 
-# class ExperienceAdmin(admin.ModelAdmin):
-#
-#    prepopulated_fields = {"slug": ("name",)}
-
 
 class ExperienceAdmin(SummernoteModelAdmin):
 
-    summernote_fields = ('description')
+    list_display = ('name', 'city', 'category')
+    search_fields = ('city', 'category')
     prepopulated_fields = {"slug": ("name",)}
+    list_filter = ('city', 'category')
+    summernote_fields = ('description')
 
 
 admin.site.register(Experience, ExperienceAdmin)
-admin.site.register(Review)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'experience', 'rating')
+    search_fields = ('experience', 'username')
+    list_filter = ('experience', 'rating')
+
+
+admin.site.register(Review, ReviewAdmin)
