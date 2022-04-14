ed341f685bb6fbe54abca119fefa193c6d92d486
Lasserini <lasserini@gmail.com>
2022-04-14 11:58:04 +0000

    Add scroll buttons to experience page

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,12 @@ function navBarToggle() {
         x.style.display = "block";
         }
     }
+
+// Functionality for Results page, Scroll to Top button
+let topButton = document.querySelector(".top_button");
+topButton.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+// Functionality for Results page, Scroll to Bottom button
+let bottomButton = document.querySelector(".bottom_button");
+bottomButton.onclick = () => window.scrollTo({ top: 10000, behavior: "smooth" });
+
