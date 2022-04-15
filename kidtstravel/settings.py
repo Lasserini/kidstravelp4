5be68a33eefefd4c069edae640920508bfabab43
Lasserini <lasserini@gmail.com>
2022-04-15 02:32:10 +0000

    Preparation for Heroku Deployment

--- kidtstravel/settings.py
+++ kidtstravel/settings.py
@@ -30,10 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # The line below has to be un-commented when turning in the project
-# X_FRAME_OPTIONS = 'SAMEORIGIN'    
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['kids-travel.herokuapp.com', 'localhost']
 
