3e68f54cb809ae8107879761efe00e6ae41f8c31
Lasserini <lasserini@gmail.com>
2022-05-27 17:34:38 +0000

    Add Delete Confirm Modals

--- static/js/script.js
+++ static/js/script.js
@@ -9,26 +9,34 @@ function navBarToggle() {
     }
 
 // Functionality for Results page, Scroll to Top button
-let topButton = document.querySelector(".top_button");
-topButton.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+// let topButton = document.querySelector(".top_button");
+// topButton.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
 // Functionality for Results page, Scroll to Bottom button
-let bottomButton = document.querySelector(".bottom_button");
-bottomButton.onclick = () => window.scrollTo({ top: 10000, behavior: "smooth" });
+// let bottomButton = document.querySelector(".bottom_button");
+// bottomButton.onclick = () => window.scrollTo({ top: 10000, behavior: "smooth" });
 
-// Functions that produce a confirmation box before any deleting is done
-function deleteConfirm(){
-    var deleteButton= document.getElementById("delete_review");
-    if (confirm("Confirm Review Deletion")){
-        deleteButton.click();
-        alert("Review Deleted!");
-    }
+// Functions that produce a confirmation box before any deletion is done
+var modal = document.getElementById("deleteExpModal");
+var modal2 = document.getElementById("deleteReviewModal");
+var btn = document.getElementById("ExpModalBtn");
+var btn2 = document.getElementById("ReviewModalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-function deleteConfirmTwo(){
-    var deleteButton= document.getElementById("delete_experience");
-    if (confirm("Careful! Confirm Experience Deletion??")){
-        deleteButton.click();
-        alert("Experience Deleted!");
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
 }
