abb43c693f31ca5baf1237de9df0c6a04ec603bb
Lasserini <lasserini@gmail.com>
2022-05-27 18:08:53 +0000

    Javascript Refactoring

--- static/js/script.js
+++ static/js/script.js
@@ -8,35 +8,43 @@ function navBarToggle() {
         }
     }
 
-// Functionality for Results page, Scroll to Top button
-// let topButton = document.querySelector(".top_button");
-// topButton.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+// Functionality for Results page Scroll Buttons
+const topButton = document.getElementsByClassName("top_button");
+topButton.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
-// Functionality for Results page, Scroll to Bottom button
-// let bottomButton = document.querySelector(".bottom_button");
-// bottomButton.onclick = () => window.scrollTo({ top: 10000, behavior: "smooth" });
+const bottomButton = document.getElementsByClassName("bottom_button");
+bottomButton.onclick = () => window.scrollTo({ top: 10000, behavior: "smooth" });
+
 
 // Functions that produce a confirmation box before any deletion is done
-var modal = document.getElementById("deleteExpModal");
-var modal2 = document.getElementById("deleteReviewModal");
-var btn = document.getElementById("ExpModalBtn");
-var btn2 = document.getElementById("ReviewModalBtn");
-var span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("deleteExpModal");
+const modal2 = document.getElementById("deleteReviewModal");
+const btn = document.getElementById("ExpModalBtn");
+const btn2 = document.getElementById("ReviewModalBtn");
+const closeModal = document.getElementsByClassName("close")[0];
+const closeModal2 = document.getElementsByClassName("close2")[0];
 
-btn.onclick = function() {
+btn.onclick = () => {
     modal.style.display = "block";
 }
 
-btn2.onclick = function() {
+btn2.onclick = () => {
     modal2.style.display = "block";
 }
 
-span.onclick = function() {
+closeModal.onclick = () => {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+closeModal2.onclick = () => {
+    modal2.style.display = "none";
+}
+
+window.onclick = (event) => {
     if (event.target == modal) {
       modal.style.display = "none";
     }
+    else if (event.target == modal2) {
+        modal2.style.display = "none";
+      }
 }
